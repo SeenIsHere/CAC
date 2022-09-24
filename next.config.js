b657ee56c38d49b21fe0c006c1a31ea45dc95d09
SeenIsHere <sdevine815@gmail.com>
2022-09-24 00:51:38 -0400

    new scopes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const searchParams = new URLSearchParams({
   client_id: process.env.SPOTIFY_CLIENT_ID,
   redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-  scope: ["user-top-read", "user-library-read"].join("%20"),
+  scope: ["user-top-read", "user-library-read", "user-read-recently-played"].join("%20"),
   response_type: "code",
   show_dialog: "true"
 })
